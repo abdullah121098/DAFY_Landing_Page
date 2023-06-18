d96cd6bff24c0df2219ddd030cedf75240eed0e0
abdullah <abdullah.bca98@gmail.com>
2023-06-18 19:18:23 +0530

    re-sizing the photo and testimonial

--- assert/js/video.js
+++ assert/js/video.js
@@ -1,60 +1,54 @@
-  // Get the video player element
-                    const videoPlayer = document.getElementById("videoPlayer");
+// Get the video player element
+const videoPlayer = document.getElementById("videoPlayer");
 
-                    // Define the video sources
-                    const videoSources = [
-                        "assert/video/01_1.mp4",
-                        "assert/video/02-2.mp4"
-                    ];
+// Define the video sources
+const videoSources = [
+    "assert/video/01_1.mp4",
+    "assert/video/02-2.mp4"
+];
 
-                    // Set the initial source index
-                    let currentSourceIndex = 0;
+// Set the initial source index
+let currentSourceIndex = 0;
 
-                    // Function to play the next video source
-                    function playNextVideo() {
-                        // Set the source of the video player
-                        videoPlayer.src = videoSources[currentSourceIndex];
+// Function to play the next video source
+function playNextVideo() {
+    // Set the source of the video player
+    videoPlayer.src = videoSources[currentSourceIndex];
 
-                        // Increment the source index
-                        currentSourceIndex = (currentSourceIndex + 1) % videoSources.length;
-                    }
+    // Increment the source index
+    currentSourceIndex = (currentSourceIndex + 1) % videoSources.length;
+}
 
-                    // Event listener for when the video ends
-                    videoPlayer.addEventListener("ended", playNextVideo);
+// Function to stop the video playback
+function stopVideo() {
+    videoPlayer.pause();
+    videoPlayer.currentTime = 0;
+    videoPlayer.src = "";
+}
 
-                    // Event listener for the modal show event
-                    const videoModal = document.getElementById("videoModal");
-                    videoModal.addEventListener("show.bs.modal", () => {
-                        // Start playing the videos when the modal is shown
-                        playNextVideo();
-                    });
+// Event listener for the play button
+const playButton = document.getElementById("playButton");
+playButton.addEventListener("click", () => {
+    // Start playing the videos when the play button is clicked
+    playNextVideo();
+});
 
-                    // Event listener for the modal close button
-                    const modalCloseButton = document.querySelector(".btn-close");
-                    modalCloseButton.addEventListener("click", () => {
-                        // Pause the video when the modal is closed
-                        videoPlayer.pause();
-                        videoPlayer.src = "";
-                    });
+// Event listener for the modal close button
+const modalCloseButton = document.querySelector(".btn-close");
+modalCloseButton.addEventListener("click", () => {
+    // Stop the video when the modal is closed
+    stopVideo();
+});
 
-                    $(document).ready(function() {
-                        var testimonialCarousel = $(".testimonial-carousel");
-        
-                        testimonialCarousel.owlCarousel({
-                            items: 1,
-                            loop: true,
-                            autoplay: true,
-                            autoplayTimeout: 50, // Adjust the value to control the speed (in milliseconds)
-                            autoplayHoverPause: true
-                        });
-        
-                        testimonialCarousel.on('mouseover', '.owl-item', function() {
-                            testimonialCarousel.trigger('stop.owl.autoplay');
-                            $(this).find('.testimonial-item').css('height', 'auto');
-                        });
-        
-                        testimonialCarousel.on('mouseout', '.owl-item', function() {
-                            testimonialCarousel.trigger('play.owl.autoplay');
-                            $(this).find('.testimonial-item').css('height', '');
-                        });
-                        });
+// Event listener for the mouseenter event on the video player
+videoPlayer.addEventListener("mouseenter", () => {
+    // Stop the video when the mouse enters the video area
+    stopVideo();
+});
+
+// Event listener for the modal show event
+const videoModal = document.getElementById("videoModal");
+videoModal.addEventListener("show.bs.modal", () => {
+    // Stop the video when the modal is shown
+    stopVideo();
+});
--- backend/export.js
+++ backend/export.js
@@ -26,6 +26,71 @@
 
           var link = document.createElement('a');
           link.href = 'data:text/csv;charset=utf-8,' + encodeURIComponent(csvContent);
-          link.download = 'report.csv';
+          link.download = 'new_ride.csv';
           link.click();
         }
+
+        function exportToExcel1() {
+          var table = document.getElementById('data-table');
+          var rows = table.getElementsByTagName('tr');
+          var csvContent = '';
+
+          for (var i = 0; i < rows.length; i++) {
+            var cells = rows[i].getElementsByTagName('td');
+            var row = [];
+
+            for (var j = 0; j < cells.length; j++) {
+              var cellData = cells[j].textContent.trim();
+              if (/^[0-9.]+$/.test(cellData)) { // Check if data is numeric
+                row.push('\t' + cellData); // Align numeric data to the right
+              } else {
+                row.push(cellData);
+              }
+            }
+
+            var thCells = rows[i].getElementsByTagName('th');
+            for (var j = 0; j < thCells.length; j++) {
+              row.push(thCells[j].textContent.trim());
+            }
+
+            csvContent += row.join(',') + '\r\n';
+          }
+
+          var link = document.createElement('a');
+          link.href = 'data:text/csv;charset=utf-8,' + encodeURIComponent(csvContent);
+          link.download = 'cancel_ride.csv';
+          link.click();
+        }
+
+        function exportToExcel3() {
+          var table = document.getElementById('data-table');
+          var rows = table.getElementsByTagName('tr');
+          var csvContent = '';
+
+          for (var i = 0; i < rows.length; i++) {
+            var cells = rows[i].getElementsByTagName('td');
+            var row = [];
+
+            for (var j = 0; j < cells.length; j++) {
+              var cellData = cells[j].textContent.trim();
+              if (/^[0-9.]+$/.test(cellData)) { // Check if data is numeric
+                row.push('\t' + cellData); // Align numeric data to the right
+              } else {
+                row.push(cellData);
+              }
+            }
+
+            var thCells = rows[i].getElementsByTagName('th');
+            for (var j = 0; j < thCells.length; j++) {
+              row.push(thCells[j].textContent.trim());
+            }
+
+            csvContent += row.join(',') + '\r\n';
+          }
+
+          var link = document.createElement('a');
+          link.href = 'data:text/csv;charset=utf-8,' + encodeURIComponent(csvContent);
+          link.download = 'complete_ride.csv';
+          link.click();
+        }
+        
